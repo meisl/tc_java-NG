e0260469e69645a0c590009247ff3a38dbd5c22f
Matthias Kling <m.kling@sprax.eu>
2014-01-19 17:21:43 +0100

    move orig tc-apis-1.7.jar to vendor/tc_java

--- build.bat
+++ build.bat
@@ -1,34 +1,36 @@
 @echo off
-rem set JAR_NAME=NtfsStreamsJ.jar
+
 set JAVA_HOME=c:\Programme\Java\jdk1.7.0_25
 set JAVALIB=%COMMANDER_PATH%\javalib
-set MY_CLASS_PATH=vendor\tc_java\tc-apis-1.7.jar;%JAVALIB%\swt-win32-3.1.2.jar;%JAVALIB%\commons-logging-api-1.0.4.jar
 
+set MY_CLASS_PATH=vendor\tc_java\tc-apis-1.7.jar;%JAVALIB%\swt-win32-3.1.2.jar;%JAVALIB%\commons-logging-api-1.0.4.jar
 set JAR=%JAVA_HOME%\bin\jar
+
+mkdir bin 2>NUL
 del /S /Q bin >NUL
+mkdir dist 2>NUL
+del dist\tc-apis-NG.jar 2>NUL
+
 %JAVA_HOME%\bin\javac -Xlint -cp %MY_CLASS_PATH% -sourcepath src -d bin src\*.java
 IF ERRORLEVEL 1 (
-  del "%JAR_NAME%" 2>NUL
   GOTO DONE
 )
 
-rem %JAR% cf "%JAR_NAME%" -C bin .
-rem del /S /Q bin >NUL
-IF x%1==xtest (
+copy /Y vendor\tc_java\tc-apis-1.7.jar dist\tc-apis-NG.jar >NUL
+%JAR% uf dist\tc-apis-NG.jar -C bin plugins
+rmdir /S /Q bin\plugins >NUL
+
+
+IF "%1"=="test" (
   cd test
   createTestFiles.bat
   cd ..
   %JAVA_HOME%\bin\java -cp %MY_CLASS_PATH%;%JAR_NAME% Main %2 %3 %4 %5 %6 %7 %8 %9
 )
 
-IF x%1==xdist (
+IF "%1"=="dist" (
+  del /S /Q dist\*.zip >NUL
   mkdir dist\temp 2>NUL
-  del /S /Q dist >NUL
-  del /S /Q dist\temp >NUL
-
-  copy /Y vendor\tc_java\tc-apis-1.7.jar dist\tc-apis-NG.jar >NUL
-  %JAR% uf dist\tc-apis-NG.jar -C bin plugins
-  rmdir /S /Q bin\plugins >NUL
 
   set PLUGIN_NAME=NtfsStreamsJ
   set PLUGIN_TYPE=WDX
